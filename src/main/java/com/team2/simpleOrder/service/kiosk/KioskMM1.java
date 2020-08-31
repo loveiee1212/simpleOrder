ea8182926934669f964a499deb6088ef59d569dd
kjb9027 <kjb9027@naver.com>
2020-08-31 20:14:51 +0900

    키오스크 무빙 컨트롤러, 리뷰 페이지 커밋
    키오스크 무빙 컨트롤러 정상작동
    키오스크 리뷰 불러오는것 뷰페이지 콘솔까지 찍힘

--- src/main/java/com/team2/simpleOrder/service/kiosk/KioskMM1.java
+++ src/main/java/com/team2/simpleOrder/service/kiosk/KioskMM1.java
@@ -4,30 +4,25 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.team2.simpleOrder.dao.kiosk.IKioskDao1;
 import com.team2.simpleOrder.dto.Review;
 
 @Service
 public class KioskMM1 {
-	
 	@Autowired
 	private IKioskDao1 kDao1;
 	
-//	@Autowired
-//	private IKioskDao2 kDao2;
-	
-	ModelAndView mav;
-
 	public List<Review> getReviewList(String c_code) {
-		
-		List<Review> rList= null;
-		rList= kDao1.getReviewList(c_code);
+		System.out.println("service on");
+		List<Review> rList= kDao1.getReviewList(c_code);
 		System.out.println(rList);
-//		System.out.println(rList);
+//		for(int i=0; i<rList.size(); i++) {
+//			System.out.println(rList.get(i).toString());
+//		}
+//		System.out.println("for문 돌림");
 		
-		return null; 
+		return rList; 
 	}
 	
 }
