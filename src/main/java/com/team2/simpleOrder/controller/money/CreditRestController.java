aa21b9dea47bafdfcbb2a4b25bf4acca77cf7f30
doremi2 <68537261+doremi2@users.noreply.github.com>
2020-09-25 13:09:36 +0900

    외상내역리스트 조회 완료 2020.09.25
    외상리스트 조회 완료 / 외상 상세출력 작업진행중 2020.09.25

---
+++ src/main/java/com/team2/simpleOrder/controller/money/CreditRestController.java
@@ -0,0 +1,28 @@
+package com.team2.simpleOrder.controller.money;
+
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.team2.simpleOrder.dto.Order;
+import com.team2.simpleOrder.service.money.CreditMM;
+
+@RestController
+@RequestMapping("/rest")
+public class CreditRestController {
+
+	@Autowired
+	CreditMM cm;
+	
+	@RequestMapping(value = "/getcreditlist", method = RequestMethod.POST)
+	public HashMap<String, String> getcreditList(HttpSession session, int oac_status) {
+		HashMap<String, String> hMap = new HashMap<String, String>();
+		return cm.getcreditList(session,oac_status);
+	}
+}
