f75d89ad56253cfe888e60bac8c8559edb038b21
LightComeP <guswns2fkrh2@Naver.com>
2020-09-25 14:19:10 +0900

    요청 변경 사항 수정진행중
    요청 변경 사항 수정진행중

--- src/main/java/com/team2/simpleOrder/dao/kiosk/KioskLoginAndSettingDao.java
+++ src/main/java/com/team2/simpleOrder/dao/kiosk/KioskLoginAndSettingDao.java
@@ -1,5 +1,6 @@
 package com.team2.simpleOrder.dao.kiosk;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.apache.ibatis.annotations.Select;
@@ -8,5 +9,8 @@ public interface KioskLoginAndSettingDao {
 
 	@Select("SELECT C_SECURITYCODE FROM COMPANY WHERE C_CODE = #{c_code}")
 	String getSecurityCode(HashMap<String, String> securityCode);
+	
+	@Select("SELECT RQ_NUM, RQ_KIND FROM REQUEST WHERE C_CODE = #{value}")
+	ArrayList<HashMap<String, Object>> getRequsetList(String c_code);
 
 }
--- src/main/webapp/WEB-INF/views/kioskSetting/requsetListSettingFrm.jsp
+++ src/main/webapp/WEB-INF/views/kioskSetting/requsetListSettingFrm.jsp
@@ -9,13 +9,19 @@
 </head>
 <body>
 	<h2>requsetListSettingFrm</h2>
+	<form action="updateRequestList" method="post">
+	<div id="reqList"></div>
+	<input type="submit" value ='요청사항 변경'>
+	</form>
 
 </body>
 <script type="text/javascript">
 $.ajax({
-	url : "getRequsetList",
+	url : "rest/getRequsetList",
+	dataType : "json",
 	success : function(data){
-		console.log(data);
+		console.log(data)
+		$("#reqList").append(data.reqList);
 	}
 });
 
